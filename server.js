09dafb6f52cc4537cdce9e2e38a1f581f1e47126
unknown <james.rock.alt@gmail.com>
2019-05-23 00:25:36 -0400

    otro ejemplo

--- server.js
+++ server.js
@@ -13,3 +13,17 @@ res.sendFile(path.join(__dirname+'/dist/material-router/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+// Heroku automagically gives us SSL
+// Lets write some middleware to redirect us
+let env = process.env.NODE_ENV || 'development';
+
+let forceSSL = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+if (env === 'production') {
+  app.use(forceSSL);
+}
